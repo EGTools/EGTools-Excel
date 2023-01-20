e4e5dce31f4c5a97e334d0aff7364754c0e529ca
EGTools <33211880+EGTools@users.noreply.github.com>
2023-01-20 11:33:56 +0900

    Help Pages 1st test
    Help file pages first upload

---
+++ docs/SearchIndex.js
@@ -0,0 +1,230 @@
+[
+  {
+    "id": "CONFIG",
+    "title": "EGTools",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "INDEX",
+    "title": "EGTools",
+    "body": "<p>EGTools는 Excel 사용에 유용한 추가기능입니다.</p>\r\n<p>\r\n    <ul>\r\n    <li>업무에 유용한 기능을 제공</li>\r\n    <li>Excel 신규 버전의 호환함수 제공</li>\r\n    <li>EGTools만의 편리한 전용함수 제공</li>\r\n    </ul>\r\n</p>\r\n<p><<%= TopicLink([주요 기능 소개],[_6J10KYVJJ]) %>></P>\r\n<p><<%= TopicLink([Ribbon Menu 소개],[_6J10KZF5Q]) %>></p>\r\n<p><<%= TopicLink([EGTools 전용함수],[_6J10LALL9]) %>></p>\r\n<p>Excel 신규 호환함수</p>\r\n<p>Google Sheet 호환함수</p>\r\n\r\n<p>\r\n    EGTools 다운로드\r\n    Excel 추가기능 설치하기\r\n</p>",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10KYVJJ",
+    "title": "주요 기능 소개",
+    "body": "<!-- HTML meta refresh URL redirection -->\r\n<html>\r\n<head>\r\n   <meta http-equiv=\"refresh\"\r\n   content=\"0; url=https://egtools.tistory.com/entry/EGTools-Manual\">\r\n</head>\r\n<body>\r\n   <p>The page has moved to:\r\n   <a href=\"https://egtools.tistory.com/entry/EGTools-Manual\">egtools.tistory.com</a></p>\r\n</body>\r\n</html>",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10KZF5Q",
+    "title": "Ribbon Menu 소개",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10L6NBN",
+    "title": "보이는 셀 복사",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10L70QF",
+    "title": "셀 병합/나누기",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10L7GGJ",
+    "title": "사진/그림 관리",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10L852V",
+    "title": "달력/일정표 만들기",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10L8N7K",
+    "title": "다단계 선택 만들기",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10L8WDH",
+    "title": "표 변환",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LALL9",
+    "title": "EGTools 전용함수",
+    "body": "\r\n<ol>\r\n<li> <%= TopicLink([검색 함수],[검색 함수]) %> </li>\r\n<li> <%= TopicLink([문자열 함수],[문자열 함수]) %> </li>\r\n<li> <%= TopicLink([색상 함수],[색상 함수]) %> </li>\r\n<li> <%= TopicLink([날짜시간 함수],[날짜시간 함수]) %> </li>\r\n<li> <%= TopicLink([계산 및 집계 함수],[계산 및 집계 함수]) %> </li>\r\n<li> <%= TopicLink([바코드 함수],[바코드 함수]) %> </li>\r\n<li> <%= TopicLink([변환 함수],[변환 함수]) %> </li>\r\n</ol>\r\n\r\n\r\nStrExt\r\n\r\n\r\nCountInStr\r\nTextPick\r\niLOOKUP\r\nMonthByWeek\r\nWeekNumOfMonth\r\nQRCode\r\nCode128\r\nTextBetween\r\nToLunar\r\nToSolar\r\nHanToNumber\r\nCompareList\r\nUS32ToDec\r\nDecToUS32\r\nDateTime\r\n\r\n\r\n\r\n\r\nKoreanHolidays\r\nTextJoinIfColor\r\nTextJoinIF\r\nSample\r\n",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LFS3L",
+    "title": "EG10to36",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LGB4A",
+    "title": "NumFromXLColumn",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LK94P",
+    "title": "NumToXLColumn",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LL5MX",
+    "title": "SumIfBack",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LLEHX",
+    "title": "SumIfFont",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LNHVD",
+    "title": "검색 함수",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LNX16",
+    "title": "문자열 함수",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LODQJ",
+    "title": "계산 및 집계 함수",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LONIT",
+    "title": "바코드 함수",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LP6WX",
+    "title": "날짜시간 함수",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LPMN6",
+    "title": "색상 함수",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LPYBV",
+    "title": "변환 함수",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LUD9D",
+    "title": "CountIfBack",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LVGPI",
+    "title": "CountIFFont",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LW1EN",
+    "title": "EVAL",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LWA7R",
+    "title": "AggregateC",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LXV8Z",
+    "title": "FindFirstData",
+    "body": "<!-- HTML meta refresh URL redirection -->\r\n<html>\r\n<head>\r\n   <meta http-equiv=\"refresh\"\r\n   content=\"0; url=https://egtools.tistory.com/entry/FINDFIRSTDATA\">\r\n</head>\r\n<body>\r\n   <p>The page has moved to:\r\n   <a href=\"https://egtools.tistory.com/entry/FINDFIRSTDATA\">FINDFIRSTDATA</a></p>\r\n</body>\r\n</html>",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LY50I",
+    "title": "FindLastData",
+    "body": "<!-- HTML meta refresh URL redirection -->\r\n<html>\r\n<head>\r\n   <meta http-equiv=\"refresh\"\r\n   content=\"0; url=https://egtools.tistory.com/entry/FINDLASTDATA\">\r\n</head>\r\n<body>\r\n   <p>The page has moved to:\r\n   <a href=\"https://egtools.tistory.com/entry/FINDLASTDATA\">FINDLASTDATA</a></p>\r\n</body>\r\n</html>",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LYJZW",
+    "title": "FindIncluded",
+    "body": "<!-- HTML meta refresh URL redirection -->\r\n<html>\r\n<head>\r\n   <meta http-equiv=\"refresh\"\r\n   content=\"0; url=https://egtools.tistory.com/entry/FINDINCLUDED\">\r\n</head>\r\n<body>\r\n   <p>The page has moved to:\r\n   <a href=\"https://egtools.tistory.com/entry/FINDINCLUDED\">FINDINCLUDED</a></p>\r\n</body>\r\n</html>",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10LYSFL",
+    "title": "FindSubstring",
+    "body": "<!-- HTML meta refresh URL redirection -->\r\n<html>\r\n<head>\r\n   <meta http-equiv=\"refresh\"\r\n   content=\"0; url=https://egtools.tistory.com/entry/FINDSUBSTRING\">\r\n</head>\r\n<body>\r\n   <p>The page has moved to:\r\n   <a href=\"https://egtools.tistory.com/entry/FINDSUBSTRING\">FINDSUBSTRING</a></p>\r\n</body>\r\n</html>",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10M0IPC",
+    "title": "MatchJoin",
+    "body": "<!-- HTML meta refresh URL redirection -->\r\n<html>\r\n<head>\r\n   <meta http-equiv=\"refresh\"\r\n   content=\"0; url=https://egtools.tistory.com/entry/MATCHJOIN\">\r\n</head>\r\n<body>\r\n   <p>The page has moved to:\r\n   <a href=\"https://egtools.tistory.com/entry/MATCHJOIN\">MATCHJOIN</a></p>\r\n</body>\r\n</html>",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10M1GDR",
+    "title": "nLookUp",
+    "body": "<!-- HTML meta refresh URL redirection -->\r\n<html>\r\n<head>\r\n   <meta http-equiv=\"refresh\"\r\n   content=\"0; url=https://egtools.tistory.com/entry/NLOOKUP\">\r\n</head>\r\n<body>\r\n   <p>The page has moved to:\r\n   <a href=\"https://egtools.tistory.com/entry/NLOOKUP\">NLOOKUP</a></p>\r\n</body>\r\n</html>",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10M25D2",
+    "title": "iLookUp",
+    "body": "<!-- HTML meta refresh URL redirection -->\r\n<html>\r\n<head>\r\n   <meta http-equiv=\"refresh\"\r\n   content=\"0; url=https://egtools.tistory.com/entry/ILOOKUP\">\r\n</head>\r\n<body>\r\n   <p>The page has moved to:\r\n   <a href=\"https://egtools.tistory.com/entry/ILOOKUP\">ILOOKUP</a></p>\r\n</body>\r\n</html>",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10MNSF7",
+    "title": "CountIfColor",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10MO6GY",
+    "title": "SumIfColor",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10MOPUB",
+    "title": "DisplayColor",
+    "body": "",
+    "keywords": ""
+  },
+  {
+    "id": "_6J10NR2PD",
+    "title": "기타 도구",
+    "body": "",
+    "keywords": ""
+  }
+]
